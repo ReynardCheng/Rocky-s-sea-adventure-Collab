89fbef7302455aeae98d877f4408e5b9ab567d00
Lily <theluckofthedrawer@gmail.com>
2019-02-08 02:30:38 +0800

    beta tut changes
    change ship
    references to rocky, shippos, camera switch etc etc
    ui changes
    dialogue scripts (incomplete)

---
+++ Rocky's Sea Adventure 1.45/Assets/DialogueSystem.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+
+    /// <summary>
+    /// This script is the dialogue system etc
+    /// I don't 100% get the script either so avoid editing the script unless you know what you're touching -ly
+    /// </summary>
+
+    public static DialogueSystem instance;
+    public ELEMENTS elements;
+
+    public float textSpeed;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    /// <summary>
+    /// Say something and show it on the speech box.
+    /// </summary>
+    public void Say(string speech, string speaker = "")
+    {
+        StopSpeaking();
+
+        speaking = StartCoroutine(Speaking(speech, false, speaker));
+    }
+
+
+    public void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+    public bool isSpeaking { get { return speaking != null; } }
+    [HideInInspector] public bool isWaitingForUserInput = false;
+
+    public string targetSpeech = "";
+    Coroutine speaking = null;
+    IEnumerator Speaking(string speech, bool additive, string speaker = "")
+    {
+        //speechPanel.SetActive(true);
+        targetSpeech = speech;
+
+        if (!additive)
+            speechText.text = "";
+        else
+            targetSpeech = speechText.text + targetSpeech;
+
+        speakerNameText.text = DetermineSpeaker(speaker);//temporary
+
+        isWaitingForUserInput = false;
+
+        while (speechText.text != targetSpeech)
+        {
+            speechText.text += targetSpeech[speechText.text.Length];
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        //text finished
+        isWaitingForUserInput = true;
+        while (isWaitingForUserInput)
+            yield return new WaitForEndOfFrame();
+
+        StopSpeaking();
+    }
+
+    string DetermineSpeaker(string s)
+    {
+        string retVal = speakerNameText.text;//default return is the current name
+        if (s != speakerNameText.text && s != "")
+            retVal = (s.ToLower().Contains("narrator")) ? "" : s;
+
+        return retVal;
+    }
+
+    [System.Serializable]
+    public class ELEMENTS
+    {
+        /// <summary>
+        /// The main panel containing all dialogue related elements on the UI
+        /// </summary>
+        //public GameObject speechPanel;
+        public Text speakerNameText;
+        public Text speechText;
+    }
+    //public GameObject speechPanel { get { return elements.speechPanel; } }
+    public Text speakerNameText { get { return elements.speakerNameText; } }
+    public Text speechText { get { return elements.speechText; } }
+}
--- Rocky's Sea Adventure 1.45/Assets/Scripts/CutSceneController.cs
+++ Rocky's Sea Adventure 1.45/Assets/Scripts/CutSceneController.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutSceneController : MonoBehaviour {
+
+    public GameObject ShipUIMenu;
+    public GameObject UIStuff;
+    public GameObject CannonUI;
+
+    public GameObject DialogueThing;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +21,24 @@ public class CutSceneController : MonoBehaviour {
 		
 	}
 
-	public void offThisCam()
+    private void Awake()
+    {
+        //set all the UI stuff false during cutscene
+        ShipUIMenu.SetActive(false);
+        UIStuff.SetActive(false);
+        CannonUI.SetActive(false);
+    }
+
+    public void offThisCam()
 	{
 		gameObject.GetComponent<Camera>().enabled = false;
-	}
+
+        //set ui stuff back on 
+        ShipUIMenu.SetActive(true);
+        UIStuff.SetActive(true);
+        CannonUI.SetActive(true);
+
+        //Dialogue lets go
+        DialogueThing.SetActive(true);
+    }
 }
