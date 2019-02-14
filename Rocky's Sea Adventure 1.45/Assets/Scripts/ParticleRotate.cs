dc1429a4ee08a90645736e871384e243162000ed
Reynard <reynardcheng@gmail.com>
2019-02-14 18:07:53 +0800

    boss particle changed

---
+++ Rocky's Sea Adventure 1.45/Assets/Scripts/ParticleRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRotate : MonoBehaviour {
+
+	public Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(speed * Time.deltaTime);
+
+	}
+}
